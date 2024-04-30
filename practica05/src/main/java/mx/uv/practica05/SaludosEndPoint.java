120b04ec4502eed04d92614604d8f2a7ebb4c46f
prunzinho <cdgn_17@hotmail.com>
2024-04-30 12:28:51 -0600

    practica05

--- practica05/src/main/java/mx/uv/practica05/SaludosEndPoint.java
+++ practica05/src/main/java/mx/uv/practica05/SaludosEndPoint.java
@@ -1,28 +1,51 @@
 package mx.uv.practica05;
 
+import java.util.ArrayList;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import mx.uv.t4is.saludos.BuscarSaludosResponse;
 import mx.uv.t4is.saludos.SaludarRequest;
 import mx.uv.t4is.saludos.SaludarResponse;
+import mx.uv.t4is.saludos.BuscarSaludosResponse.Saludos;
 
 @Endpoint
 //
 
 public class SaludosEndPoint {
     // Adjunta tambien al metoodo al que se esta apuntado
+    ArrayList<Saludos> saludos = new ArrayList<>();
+
     @PayloadRoot(localPart = "SaludarRequest", namespace = "t4is.uv.mx/saludos")
 
     @ResponsePayload // La respuesta automaticamnente la convierte en el formato que corresponda
                      // //Payload de 2 tipos
+
     public SaludarResponse saludar(@RequestPayload SaludarRequest peticion) {
         SaludarResponse respuesta = new SaludarResponse();
+
         respuesta.setRespuesta("hola" + peticion.getNombre());
+
+        Saludos e = new Saludos();
+        e.setNombre(peticion.getNombre());
+        saludos.add(e); // add(peticion.getNombre());
         return respuesta;
     }
 
-    //
+    @PayloadRoot(localPart = "BuscarSaludosRequest", namespace = "t4is.uv.mx/saludos")
+
+    @ResponsePayload
+    public BuscarSaludosResponse buscarSaludos() {
+        BuscarSaludosResponse respuesta = new BuscarSaludosResponse();
+        // respuesta.getSaludos().add(saludos.get);
+        for (int i = 0; i < saludos.size(); i++) {
+            respuesta.getSaludos().add(saludos.get(i));
+        }
+
+        return respuesta;
+    }
 
 }
